53626f5779f74333e090cd1b45d8021841f9b547
Anna Jaunzeme <anna.jaunzeme@inbox.lv>
2021-10-23 23:24:03 +0300

    done

---
+++ duplicates/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter size of the array: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int[] usersArray = new int[a];
+            int i, j, count = 0;
+           
+            Console.WriteLine("Enter elements in the array: ");
+              for (i = 0; i < a; i++)
+                
+            {
+               usersArray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("\n \n Thank You!");
+            for (i = 0; i < a ; i++)
+            {
+              for (j = i + 1; j < a; j++)
+                {
+                    
+                    if (usersArray[i] == usersArray[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+            if (count > 0)
+            {
+             Console.WriteLine("\n Total number of duplicate elements found in array:" + count);
+            }
+            else
+            {
+                Console.WriteLine("\n There is not any duplicates in your Array! ");
+            }
+                                Console.ReadLine();
+        }
+    }
+}
